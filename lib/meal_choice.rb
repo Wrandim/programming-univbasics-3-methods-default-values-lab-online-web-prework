25126f240a8c8488c0c17bcdf47e51267eb411c2
William Rauschendorfer <dragonstransport@yahoo.com>
2019-08-13 15:46:57 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,7 +3,7 @@
 def meal_choice(veg1 , veg2 , protein = "meat")
   puts "What a nutritious meal!" and your order to the console
   
-  return "A plate of #{protein} with #{veg1} and #{veg2}."
+  phrase = "A plate of #{protein} with #{veg1} and #{veg2}."
   
 end
 
